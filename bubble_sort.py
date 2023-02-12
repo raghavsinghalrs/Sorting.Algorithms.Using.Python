c752a3dd429305cf64cd8e52021e8065b693c060
Raghav Singhal <84004096+raghavsinghalrs@users.noreply.github.com>
2023-02-13 02:16:20 +0530

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,21 +1,7 @@
-list =  [10,7,5,6,-1]
-n = len(list)-1
-a=b=0
-def bubbleSort(list,a,b):
-    i=0
-    j=i+1
-    while (i<(len(list)-a) and j<(len(list)-b)):
-        if list[i]>list[j]:
-            list[i],list[j]=list[j],list[i]
-            i+=1 
-            j+=1  
-        else:
-            i+=1 
-            j+=1
-    return list
-while(n>0):
-    list = bubbleSort(list,a,b)
-    a+=1
-    b+=1
-    n-=1
-print(list)
+a = [7,9,13,12,6]
+
+for i in range(0,len(a)-1):
+    for j in range(0,len(a)-1-i):
+        if a[j]>a[j+1]:
+            a[j],a[j+1]=a[j+1],a[j]
+print(a)
